431c8c8bad50192e529355b2b05fd0b9ad362698
Kareem Kadry <cr7.rm35@gmail.com>
2026-03-16 21:41:37 +0200

    fix seeder constraint issue

--- App/views/Order/Components/head.php
+++ App/views/Order/Components/head.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <style>
         :root {
-            --cafe-primary: #1a4d2e;
+            --cafe-primary: #0d6efd;
             --cafe-secondary: #e8dfca;
         }
 
